1d0e4c69e5c1547a76f82406dd72aacecea0530b
Rodrigo <Rodrigo.gerzur@educa.jcyl.es>
2021-10-19 10:17:24 +0200

    Borrado

--- codigoPHP/ejercicio05.php
+++ codigoPHP/ejercicio05.php
@@ -1,8 +1,19 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio05.php</title>
+    </head>
+    <body>
+        <?php
+            
+            $fechaStamp = new DateTime();
+            echo "El valor del timeStamp de la variable fecha que cree es ".$fechaStamp->getTimestamp();
+        ?>
+    </body>
+</html>
